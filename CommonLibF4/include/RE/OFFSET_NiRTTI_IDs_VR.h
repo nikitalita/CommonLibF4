fc4a32389730ba6aa8ef1196692c320a836d0278
nikitalita <69168929+nikitalita@users.noreply.github.com>
2023-02-28 13:13:31 -0800

    wip fixes

--- CommonLibF4/include/RE/OFFSET_NiRTTI_IDs_VR.h
+++ CommonLibF4/include/RE/OFFSET_NiRTTI_IDs_VR.h
@@ -2,35 +2,35 @@
 
 namespace RE
 {
-	namespace Ni_RTTI
+	namespace Ni_RTTI_VR
 	{
-		inline constexpr REL::OFFSET BSCullingProcess{ 0x60f0b38 };
-		inline constexpr REL::OFFSET BSFaceGenMorphData{ 0x5a62100 };
-		inline constexpr REL::OFFSET BSFaceGenMorphDataHair{ 0x5a62120 };
-		inline constexpr REL::OFFSET BSFaceGenMorphDataHead{ 0x5a62110 };
-		inline constexpr REL::OFFSET BSFadeNodeCuller{ 0x689adc8 };
-		inline constexpr REL::OFFSET BSGeometryListCullingProcess{ 0x689e178 };
-		inline constexpr REL::OFFSET BSParabolicCullingProcess{ 0x689e168 };
-		inline constexpr REL::OFFSET BSTempEffect{ 0x5a671d8 };
-		inline constexpr REL::OFFSET BSTempEffectDebris{ 0x5a67270 };
-		inline constexpr REL::OFFSET BSTempEffectGeometryDecal{ 0x5a672a8 };
-		inline constexpr REL::OFFSET BSTempEffectParticle{ 0x5a672c0 };
-		inline constexpr REL::OFFSET BSTempEffectSPG{ 0x5a672f0 };
-		inline constexpr REL::OFFSET BSTempEffectScreenSpaceDecal{ 0x59366d0 };
-		inline constexpr REL::OFFSET ModelReferenceEffect{ 0x5acd150 };
-		inline constexpr REL::OFFSET NiCullingProcess{ 0x5c62c00 };
-		inline constexpr REL::OFFSET ReferenceEffect{ 0x5acd338 };
-		inline constexpr REL::OFFSET ShaderReferenceEffect{ 0x5acd440 };
-		inline constexpr REL::OFFSET SummonPlacementEffect{ 0x5acd5e0 };
-		inline constexpr REL::OFFSET bhkCharacterProxy{ 0x65d8498 };
-		inline constexpr REL::OFFSET bhkCharacterRigidBody{ 0x65d84c8 };
-		inline constexpr REL::OFFSET bhkNPCollisionObject{ 0x65a4e90 };
-		inline constexpr REL::OFFSET bhkNPCollisionObjectBase{ 0x65a4e70 };
-		inline constexpr REL::OFFSET bhkNPCollisionObjectUnlinked{ 0x65a4ea0 };
-		inline constexpr REL::OFFSET bhkNPCollisionProxyObject{ 0x65a4e80 };
-		inline constexpr REL::OFFSET bhkPhysicsSystem{ 0x65a4ed8 };
-		inline constexpr REL::OFFSET bhkRagdollSystem{ 0x65a4ee8 };
-		inline constexpr REL::OFFSET bhkWorld{ 0x65a3db8 };
-		inline constexpr REL::OFFSET bhkWorldM{ 0x65a53d8 };
+		inline constexpr REL::Offset BSCullingProcess{ 0x60f0b38 };
+		inline constexpr REL::Offset BSFaceGenMorphData{ 0x5a62100 };
+		inline constexpr REL::Offset BSFaceGenMorphDataHair{ 0x5a62120 };
+		inline constexpr REL::Offset BSFaceGenMorphDataHead{ 0x5a62110 };
+		inline constexpr REL::Offset BSFadeNodeCuller{ 0x689adc8 };
+		inline constexpr REL::Offset BSGeometryListCullingProcess{ 0x689e178 };
+		inline constexpr REL::Offset BSParabolicCullingProcess{ 0x689e168 };
+		inline constexpr REL::Offset BSTempEffect{ 0x5a671d8 };
+		inline constexpr REL::Offset BSTempEffectDebris{ 0x5a67270 };
+		inline constexpr REL::Offset BSTempEffectGeometryDecal{ 0x5a672a8 };
+		inline constexpr REL::Offset BSTempEffectParticle{ 0x5a672c0 };
+		inline constexpr REL::Offset BSTempEffectSPG{ 0x5a672f0 };
+		inline constexpr REL::Offset BSTempEffectScreenSpaceDecal{ 0x59366d0 };
+		inline constexpr REL::Offset ModelReferenceEffect{ 0x5acd150 };
+		inline constexpr REL::Offset NiCullingProcess{ 0x5c62c00 };
+		inline constexpr REL::Offset ReferenceEffect{ 0x5acd338 };
+		inline constexpr REL::Offset ShaderReferenceEffect{ 0x5acd440 };
+		inline constexpr REL::Offset SummonPlacementEffect{ 0x5acd5e0 };
+		inline constexpr REL::Offset bhkCharacterProxy{ 0x65d8498 };
+		inline constexpr REL::Offset bhkCharacterRigidBody{ 0x65d84c8 };
+		inline constexpr REL::Offset bhkNPCollisionObject{ 0x65a4e90 };
+		inline constexpr REL::Offset bhkNPCollisionObjectBase{ 0x65a4e70 };
+		inline constexpr REL::Offset bhkNPCollisionObjectUnlinked{ 0x65a4ea0 };
+		inline constexpr REL::Offset bhkNPCollisionProxyObject{ 0x65a4e80 };
+		inline constexpr REL::Offset bhkPhysicsSystem{ 0x65a4ed8 };
+		inline constexpr REL::Offset bhkRagdollSystem{ 0x65a4ee8 };
+		inline constexpr REL::Offset bhkWorld{ 0x65a3db8 };
+		inline constexpr REL::Offset bhkWorldM{ 0x65a53d8 };
 	}
 }
